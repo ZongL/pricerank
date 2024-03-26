53867930cdf187196aa84bb99afbb8a20dbcfe6b
andyzong <andyzong>
2024-03-26 15:31:39 +0800

    add "responsive"

--- pages/teslapatent.tsx
+++ pages/teslapatent.tsx
@@ -5,9 +5,9 @@ import { teslapatentData } from "@/app/data/PatentData";
 import 'tailwindcss/tailwind.css'; 
 
 import DataTable from 'datatables.net-dt';
-import 'datatables.net-dt/css/dataTables.dataTables.css'
-import $ from 'jquery';
-
+import 'datatables.net-dt/css/dataTables.dataTables.css';
+//import '@/app/ui/responsive.dataTables.css';  // 引入CSS文件
+import 'datatables.net-responsive/js/dataTables.responsive';
 
 export default function Patents(){
   useEffect(() => {  
@@ -18,20 +18,24 @@ export default function Patents(){
       new DataTable(table, {
         data: teslapatentData.docs,
         columns: [
-          { data: 'documentId' },
-          { data: 'datePublished' },
-          { data: 'title' },
-          { data: 'patentNumber' },
-          { data: 'inventors'},
-          { data: 'pageCount'}
-      ]
-        //scrollY: 300,
-        //paging: false
+          { data: 'documentId', orderable: false },
+          { data: 'datePublished', orderable: true },
+          { data: 'title', orderable: false },
+          { data: 'patentNumber', orderable: false },
+          { data: 'inventors', orderable: false },
+          { data: 'pageCount', orderable: false }
+      ],
+      // 设置默认排序
+      responsive: true,
+      columnDefs: [
+        { responsivePriority: 10002, targets: -1 } //最后一行折叠隐藏
+    ],
+      order: [[1, 'desc']], // 列索引1（即datePublished列）降序排序
+      paging: true
       });  
     }  
   }, []);  
   return (
-
      <main className="flex min-h-screen flex-col p-6">
       <div className="flex flex-col items-center justify-center h-52 bg-blue-500 p-4 rounded-lg md:flex-row md:h-32">
         <div className="flex items-center">
@@ -40,11 +44,11 @@ export default function Patents(){
         <div className="flex-grow"></div>
         <div className="text-white self-end">Design By ZongL</div>
       </div>
-    <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-9/10 md:px-20">
+    {/* <div className="mt-4 flex grow flex-col gap-4 md:flex-row">*/}
+        {/*<div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-9/10 md:px-20"> */}
               <div>  
                 <h1>Tesla patent</h1>  
-                <table id="myTable" className="display" style={{ width: '100%' }}>  
+                <table id="myTable" className="display nowrap" style={{ width:'100%'}}>  
                   <thead>  
                     <tr>  
                     <th>Document ID</th>
@@ -58,9 +62,9 @@ export default function Patents(){
                   </thead>  
                 </table>  
               </div>  
-        </div>    
+        {/* </div> */}
         {/* <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-1/5 md:px-20"></div>     */}
-    </div>
+    {/* </div> */}
 
 
     </main>
