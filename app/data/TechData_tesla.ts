8361d873faa2e72141081431f5ba91050b617872
andyzong <andyzong>
2024-03-29 19:44:03 +0800

    add /teslatech page

---
+++ app/data/TechData_tesla.ts
@@ -0,0 +1,22 @@
+export const teslatechData: any = 
+{
+    "cursorMarker": "AoJwgMjRm4wDNzYwMjAxNjY3IVVTLVVTLTExODM3NTIz",
+    "numFound": 1140,
+    "docs": [
+        {
+            "URL": "https://www.youtube.com/watch?v=AVCHC2cw0v0",
+            "Detail": "拆解特斯拉Model3的前车身控制器（Tesla Model 3 - Teardown of VCFRONT (Front Body Controller)",
+            "AI_Opinion": "null"
+        },
+        {
+            "URL": "https://www.youtube.com/watch?v=rDYbvI32OBE",
+            "Detail": "特斯拉Model3所有控制器拆解展示，爆炸展示图",
+            "AI_Opinion": "null"
+        },
+        {
+            "URL": "https://zhuanlan.zhihu.com/p/108892577",
+            "Detail": "知乎讲解特斯拉Model3前车身控制器，包含Pin脚定义",
+            "AI_Opinion": "null"
+        }
+    ]
+}
---
+++ pages/teslatech.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useEffect } from 'react';  
+import { teslatechData } from "@/app/data/TechData_tesla";
+
+import 'tailwindcss/tailwind.css'; 
+
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-dt/css/dataTables.dataTables.css';
+import '@/app/ui/responsive.dataTables.css';  // 引入CSS文件
+import '@/app/ui/responsive.dataTables.min.css';  // 引入CSS文件
+import 'datatables.net-responsive/js/dataTables.responsive';
+
+export default function Patents(){
+  useEffect(() => {  
+    // 确保组件已经挂载到 DOM 上  
+    const table = document.getElementById('myTable');  
+    if (table) {  
+      //初始化 DataTables  
+      new DataTable(table, {
+        data: teslatechData.docs,
+        columns: [
+          { data: 'URL', orderable: false },
+          { data: 'Detail', orderable: true },
+          { data: 'AI_Opinion', orderable: false }
+      ],
+      //order: [[1, 'desc']], // 列索引1（即datePublished列）降序排序
+      paging: true
+      });  
+    }  
+  }, []);  
+  return (
+     <main className="flex min-h-screen flex-col p-6">
+      <div className="flex flex-col items-center justify-center h-52 bg-blue-500 p-4 rounded-lg md:flex-row md:h-32">
+        {/* <div className="flex items-center">
+          <h2 className="text-white text-3xl font-bold">Car Price Overview</h2>
+        </div>
+        <div className="flex-grow"></div>
+        <div className="text-white self-end">Design By ZongL</div> */}
+      </div>
+    {/* <div className="mt-4 flex grow flex-col gap-4 md:flex-row"> */}
+        {/* <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-9/10 md:px-20"> */}
+              <div>  
+                <h1>Tesla patent</h1>  
+                <table id="myTable" className="display responsive" style={{ width:'100%'}}>  
+                  <thead>  
+                    <tr>  
+                    <th>URL</th>
+                    <th>Detail</th>
+                    <th>AI_Opinion: </th>
+                    </tr>  
+                  </thead>  
+                </table>  
+              </div>  
+        {/* </div> */}
+        {/* <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-1/5 md:px-20"></div>     */}
+    {/* </div> */}
+
+
+    </main>
+  );
+};
+
