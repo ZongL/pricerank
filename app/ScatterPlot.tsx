ddd42555c10c53b5e08c3d6d7a21e355b973b2a4
andyzong <andyzong>
2024-04-03 17:14:52 +0800

    update label style,not show label name on chart

--- app/ScatterPlot.tsx
+++ app/ScatterPlot.tsx
@@ -52,7 +52,7 @@ const ScatterPlot = () => {
         type: 'scatter',
         symbolSize: (value: number[]) => Math.log10(value[1]) * 5, // 根据价格设置点的大小
         label: {
-          show: true,
+          show: false,
           formatter: '{b}',
           position: 'top'
         },
