fad5bf96b6df0b0b98e8483334dab84fc9696ed2
andyzong <andyzong>
2024-05-07 10:20:37 +0800

    update webpage title

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Next.js</title>
+        <title>CarPriceOverview</title>
       </head>
       <body>
         {children}
